c5543c430cf4b403604ebf2167e1aa7243c2ae75
Muniabi <volansinfo@yandex.ru>
2024-03-31 18:29:06 +0300

    first commit tabs

---
+++ event-object/tabs/task.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const tabs = document.getElementById('tabs1');
+    const tabNavigation = tabs.querySelector('.tab__navigation');
+    const tabContents = tabs.querySelector('.tab__contents');
+
+    tabNavigation.addEventListener('click', (event) => {
+        if (!event.target.classList.contains('tab')) return;
+
+        const selectedTab = event.target;
+        const selectedTabIndex = Array.from(tabNavigation.children).indexOf(selectedTab);
+        const activeTabContent = tabContents.querySelector('.tab__content.tab__content_active');
+        const selectedTabContent = tabContents.children[selectedTabIndex];
+
+        if (!selectedTabContent || selectedTabContent === activeTabContent) return;
+
+        // Скрыть активное содержимое вкладки
+        activeTabContent.classList.remove('tab__content_active');
+
+        // Сделать выбранное содержимое вкладки активным
+        selectedTabContent.classList.add('tab__content_active');
+
+        // Убрать активный класс у текущей вкладки
+        const activeTab = tabNavigation.querySelector('.tab.tab_active');
+        activeTab.classList.remove('tab_active');
+
+        // Добавить активный класс к выбранной вкладке
+        selectedTab.classList.add('tab_active');
+    });
+});
