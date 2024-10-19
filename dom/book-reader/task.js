cadfa83a20b4b6b62528d893ac3b43bc54f8baf6
Muniabi <muniabi@yandex.ru>
2024-10-19 18:08:45 +0300

    fix

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,36 +1,52 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.querySelectorAll('.font-size').forEach(function (control) {
-        control.addEventListener('click', function (event) {
+document.addEventListener("DOMContentLoaded", function () {
+    const book = document.getElementById("book");
+    const fontSizeControls = document.querySelectorAll(".font-size");
+    const colorControls = document.querySelectorAll(".color");
+
+    fontSizeControls.forEach(function (control) {
+        control.addEventListener("click", function (event) {
             event.preventDefault();
-            document.querySelectorAll('.font-size').forEach(function (control) {
-                control.classList.remove('font-size_active');
+            fontSizeControls.forEach(function (control) {
+                control.classList.remove("font-size_active");
             });
-            this.classList.add('font-size_active');
-            let size = this.dataset.size;
-            document.getElementById('book').classList.remove('book_fs-small', 'book_fs-big');
-            document.getElementById('book').classList.add('book_fs-' + size);
+            this.classList.add("font-size_active");
+
+            const size = this.dataset.size;
+            book.classList.remove("book_fs-small", "book_fs-big");
+            if (size) {
+                book.classList.add("book_fs-" + size);
+            }
         });
     });
 
-    document.querySelectorAll('.color').forEach(function (control) {
-        control.addEventListener('click', function (event) {
+    colorControls.forEach(function (control) {
+        control.addEventListener("click", function (event) {
             event.preventDefault();
-            let isTextColor = this.parentElement.classList.contains('book__control_color');
-            let color = this.dataset[isTextColor ? 'textColor' : 'bgColor'];
-            let book = document.getElementById('book');
-            let className = isTextColor ? 'book_color-' : 'book_bg-';
 
-            // если цвет текста и фона совпадают, то меняем цвет текста
-            if (isTextColor && color === 'white') {
-                isTextColor = false;
+            const isTextColor = this.parentElement.classList.contains(
+                "book__control_color"
+            );
+            const color = this.dataset[isTextColor ? "textColor" : "bgColor"];
+            const className = isTextColor ? "book_color-" : "book_bg-";
+            colorControls.forEach(function (control) {
+                control.classList.remove("color_active");
+            });
+
+            this.classList.add("color_active");
+            if (isTextColor) {
+                book.classList.remove(
+                    "book_color-gray",
+                    "book_color-whitesmoke",
+                    "book_color-black"
+                );
+            } else {
+                book.classList.remove(
+                    "book_bg-gray",
+                    "book_bg-black",
+                    "book_bg-white"
+                );
             }
 
-            document.querySelectorAll('.color').forEach(function (control) {
-                control.classList.remove('color_active');
-            });
-            this.classList.add('color_active');
-
-            book.classList.remove('book_color-gray', 'book_color-whitesmoke', 'book_color-black', 'book_bg-gray', 'book_bg-black', 'book_bg-white');
             book.classList.add(className + color);
         });
     });
